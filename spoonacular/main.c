ac07a9f9f1cec7462a3f7dc6d712c4cad1d3c3ff
yhabie <yhabie@myges.fr>
2023-06-01 16:39:01 +0200

    add switch case to dynamically parse the json object

--- spoonacular/main.c
+++ spoonacular/main.c
@@ -3,48 +3,83 @@
 #include <string.h>
 #include <json-c/json.h>
 
+void parse_ingredient(json_object * jobj) {
+    enum json_type type;
+    json_object_object_foreach(jobj, key, val) {
+        type = json_object_get_type(val);
+        if (type == json_type_string && strcmp(key, "name") == 0) {
+            printf("- %s\n", json_object_get_string(val));
+        }
+    }
+}
+
+void parse_json(json_object * jobj) {
+    enum json_type type;
+    json_object_object_foreach(jobj, key, val) {
+        type = json_object_get_type(val);
+        switch (type) {
+            case json_type_string:
+                if(strcmp(key, "title") == 0) {
+                    printf("Title: %s\n", json_object_get_string(val));
+                }
+                break;
+            case json_type_array:
+                if(strcmp(key, "extendedIngredients") == 0) {
+                    printf("Ingredients:\n");
+                    for(int i=0; i < json_object_array_length(val); i++){
+                        json_object * array_item = json_object_array_get_idx(val, i);
+                        parse_ingredient(array_item);
+                    }
+                }
+                break;
+            case json_type_object:
+                parse_json(val);
+                break;
+        }
+    }
+}
+
+void parse_instructions(json_object * jobj) {
+    enum json_type type;
+    json_object_object_foreach(jobj, key, val) {
+        type = json_object_get_type(val);
+        switch (type) {
+            case json_type_array:
+                if(strcmp(key, "steps") == 0) {
+                    printf("Instructions:\n");
+                    for(int i=0; i < json_object_array_length(val); i++){
+                        json_object * array_item = json_object_array_get_idx(val, i);
+                        json_object_object_foreach(array_item, step_key, step_val) {
+                            if (json_object_get_type(step_val) == json_type_string && strcmp(step_key, "step") == 0) {
+                                printf("- %s\n", json_object_get_string(step_val));
+                            }
+                        }
+                    }
+                }
+                break;
+            case json_type_object:
+                parse_instructions(val);
+                break;
+        }
+    }
+}
+
 int main() {
     FILE *fp;
-    char buffer[1024];
+    char buffer[2048];
     fp = fopen("recipe.json", "r");
     if (fp == NULL) {
         perror("Failed to open file");
         return 1;
     }
-    fread(buffer, 1024, 1, fp);
+    size_t read_size = fread(buffer, 1, sizeof(buffer) - 1, fp);
+    buffer[read_size] = '\0';  // Ensure the string is null-terminated
     fclose(fp);
 
-    struct json_object *parsed_json = json_tokener_parse(buffer);
-    struct json_object *title_obj;
-    struct json_object *ingredients_arr;
-
-    if (!json_object_object_get_ex(parsed_json, "title", &title_obj)) {
-        fprintf(stderr, "Failed to get 'title' field from JSON object\n");
-        return 1;
-    }
-    if (!json_object_object_get_ex(parsed_json, "extendedIngredients", &ingredients_arr)) {
-        fprintf(stderr, "Failed to get 'extendedIngredients' field from JSON object\n");
-        return 1;
-    }
-    if (json_object_get_type(title_obj) != json_type_string) {
-        fprintf(stderr, "'title' field is not a string\n");
-        return 1;
-    }
-    if (json_object_get_type(ingredients_arr) != json_type_array) {
-        fprintf(stderr, "'extendedIngredients' field is not an array\n");
-        return 1;
-    }
-
-    const char *title = json_object_get_string(title_obj);
-    printf("Title: %s\n", title);
-
-    printf("Ingredients:\n");
-    int ingredients_len = json_object_array_length(ingredients_arr);
-    for (int i = 0; i < ingredients_len; i++) {
-        struct json_object *ingredient_obj = json_object_array_get_idx(ingredients_arr, i);
-        const char *ingredient = json_object_get_string(ingredient_obj);
-        printf("- %s\n", ingredient);
-    }
-
+    json_object * jobj = json_tokener_parse(buffer);
+    parse_json(jobj);
+    parse_instructions(jobj);
     return 0;
 }
+   
+ 
